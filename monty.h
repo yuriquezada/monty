429bf303032b8d7c6af90e337bf500c642401204
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-09-26 23:20:22 -0500

    Add swap() and add() functions

--- monty.h
+++ monty.h
@@ -60,13 +60,15 @@ typedef struct instruction_s
 } instruction_t;
 
 void get_op(char *op, stack_t **stack, unsigned int line_number);
-void m_push(stack_t **stack, unsigned int line_number);
-void m_pall(stack_t **stack, unsigned int line_number);
 void free_stack(int status, void *arg);
 void m_fs_close(int status, void *arg);
 void free_lineptr(int status, void *arg);
 stack_t *add_node(stack_t **stack, const int n);
+void m_push(stack_t **stack, unsigned int line_number);
+void m_pall(stack_t **stack, unsigned int line_number);
 void m_pint(stack_t **stack, unsigned int line_number);
 void m_pop(stack_t **stack, unsigned int line_number);
+void m_swap(stack_t **stack, unsigned int line_number);
+void m_add(stack_t **stack, unsigned int line_number);
 
 #endif /* _MONTY_H_ */
