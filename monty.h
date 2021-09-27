53edff28c67ad3f1e111b61f17b92778f9ff0fc6
Yuri Quezada <45071280+yuriquezada@users.noreply.github.com>
2021-09-26 23:13:53 -0500

    Add function Pop and pint

--- monty.h
+++ monty.h
@@ -66,5 +66,7 @@ void free_stack(int status, void *arg);
 void m_fs_close(int status, void *arg);
 void free_lineptr(int status, void *arg);
 stack_t *add_node(stack_t **stack, const int n);
+void m_pint(stack_t **stack, unsigned int line_number);
+void m_pop(stack_t **stack, unsigned int line_number);
 
 #endif /* _MONTY_H_ */
